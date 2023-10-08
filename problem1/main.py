1a91e3aa789176afd3a2fedad40a7cece28d4b70
Amin S <aminimnida95@gmail.com>
2023-10-08 20:13:48 +0700

    tugas done

--- problem1/main.py
+++ problem1/main.py
@@ -1,6 +1,15 @@
 def play_with_asterisk(n):
-    pattern = ""
-    return pattern
+    for i in range(1, n+1):
+        # Mencetak spasi sebanyak n-i
+        for j in range(n-i):
+            print(" ", end="")
+        # Mencetak asterisk sebanyak i
+        for k in range(i):
+            print("* ", end="")
+        # Pindah ke baris baru setelah mencetak baris i
+        print()
+
+print(play_with_asterisk)
 
 if __name__ == '__main__':
     print(play_with_asterisk(5))
--- problem2/main.py
+++ problem2/main.py
@@ -1,6 +1,18 @@
 def draw_xyz(N):
-    pattern = ""
-    return pattern
+    count = 1
+    for i in range(N):
+        for j in range(N):
+            if count % 3 == 0:
+                # untuk kelipatan tiga cetak 'X'
+                print('X', end = ' ')
+            elif count % 2 == 0:
+                # untuk kelipatan dua (genap) cetak 'Z'
+                print('Z', end = ' ')
+            else:
+                # untuk kelipatan ganjil cetak 'Y'
+                print('Y', end = ' ')
+            count += 1
+        print()
 
 if __name__ == '__main__':
     print(draw_xyz(3))
